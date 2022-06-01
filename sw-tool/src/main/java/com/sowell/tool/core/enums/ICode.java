c92b28b0ebd74e50ac72047da38f45474afe4b79
孔胜 <iskongsheng@foxmail.com>
2022-06-01 10:22:21 +0800

    ## 修改

---
+++ sw-tool/src/main/java/com/sowell/tool/core/enums/ICode.java
@@ -0,0 +1,15 @@
+package com.sowell.tool.core.enums;
+
+/**
+ * TODO
+ *
+ * @author 孔胜
+ * @version 版权 Copyright(c)2022 杭州设维信息技术有限公司
+ * @date 2022/3/28 11:55
+ */
+public interface ICode {
+
+    Integer getCode();
+
+    String getMessage();
+}
